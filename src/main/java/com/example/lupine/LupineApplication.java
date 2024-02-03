e4375f2ecf1d255e0c34c1017b3b0f6057c496e5
VeryHighMan <ipun0013@gmail.com>
2024-02-03 17:22:33 +0900

    first commit

---
+++ src/main/java/com/example/lupine/LupineApplication.java
@@ -0,0 +1,13 @@
+package com.example.lupine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LupineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lupine/LupineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lupine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
